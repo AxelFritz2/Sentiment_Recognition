08cc5b07794b52ece0aca9d113ec67bce252dfa8
AxelFritz1 <fritz.axel@outlook.com>
2023-12-20 18:51:08 +0100

    add install bash

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ VENV_DIR="venv"
 if [ -d "$VENV_DIR" ]; then
     echo "L'environnement virtuel existe déjà."
 else
-    command -v virtualenv >/dev/null 2>&1 || { echo >&2 "virtualenv n'est pas installé. Installation en cours..."; pip install virtualenv; }
+    command -v virtualenv >/dev/null 2>&1 || { echo >&2 "virtualenv n'est pas installé. Installation en cours..."; python3 pip install virtualenv; }
 
-    virtualenv $VENV_DIR
+    python3 virtualenv $VENV_DIR
 
     source $VENV_DIR/bin/activate
 
-    pip install -r requirements.txt
+    python3 pip install -r requirements.txt
 
     deactivate
 
