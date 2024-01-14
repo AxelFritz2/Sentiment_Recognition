c59ac7c532bf7dda8b568794d8784a3c3bdf75e1
AxelFritz1 <fritz.axel@outlook.com>
2024-01-14 16:02:40 +0100

    new requirements

--- data_collector_2/collect_data.sh
+++ data_collector_2/collect_data.sh
@@ -6,5 +6,5 @@ source ./venv/bin/activate
 # Exécuter le serveur Flask
 python3 ./data_collector_2/collect.py
 
-mkdir "./data"
-mv './data_collector_2/face-expression-recognition-dataset' './data'
+mkdir "data"
+mv face-expression-recognition-dataset data
