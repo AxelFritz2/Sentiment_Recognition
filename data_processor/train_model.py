ea75bfab757a6206343a509aa5df5786194a8a91
AxelFritz1 <fritz.axel@outlook.com>
2024-01-17 10:15:49 +0100

    change path to data

--- data_processor/train_model.py
+++ data_processor/train_model.py
@@ -1,3 +1,5 @@
+import os
+
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
 from keras.callbacks import EarlyStopping
@@ -13,8 +15,8 @@ train_datagen = ImageDataGenerator(rescale=1. / 255,
                                            fill_mode='nearest')
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
-
-training_set = train_datagen.flow_from_directory(directory='/Projet_Linux/./data/face-expression-recognition-dataset/images/train',
+print(os.getcwd())
+training_set = train_datagen.flow_from_directory(directory='./data/face-expression-recognition-dataset/images/train',
                                               target_size=(48, 48),
                                               class_mode='categorical',
                                               batch_size=32,
@@ -22,7 +24,7 @@ training_set = train_datagen.flow_from_directory(directory='/Projet_Linux/./data
                                                        'Happy', 'Neutral', 'Sad',
                                                        'Surprise'])
 
-test_set = test_datagen.flow_from_directory(directory='/Projet_Linux/./data/face-expression-recognition-dataset/images/validation',
+test_set = test_datagen.flow_from_directory(directory='./data/face-expression-recognition-dataset/images/validation',
                                             target_size=(48, 48),
                                             class_mode='categorical',
                                             batch_size=32,
