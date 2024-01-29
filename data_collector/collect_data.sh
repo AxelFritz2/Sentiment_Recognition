45b137b67d04c3093f36b19a90f4113b9fe62167
AxelFritz1 <fritz.axel@outlook.com>
2024-01-29 11:42:33 +0100

    add pretrained model

--- data_collector/collect_data.sh
+++ data_collector/collect_data.sh
@@ -6,7 +6,7 @@ source ./venv/bin/activate
 # Récupérer les données
 python3 ./data_collector/collect.py
 
-mkdir "data"
-chmod 777 data
+#mkdir "data"
+#chmod 777 data
 
-mv face-expression-recognition-dataset data
+#mv face-expression-recognition-dataset data
